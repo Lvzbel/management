19a94125313abe420e95a3e33e5ea1af1631a2c2
Lvzbel <rcoto@protonmail.com>
2021-01-01 23:19:20 -0800

    Added relations to all models

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends Model
 {
     use HasFactory;
+
+    protected $guaded = [];
+
+    public function user()
+    {
+        $this->belongsToMany(User::class);
+    }
 }
