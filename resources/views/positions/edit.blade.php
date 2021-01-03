22b3968192b427bcc6c4b2e5a43aa0af3fd525e4
Lvzbel <rcoto@protonmail.com>
2021-01-02 20:34:33 -0800

    Views for postions

---
+++ resources/views/positions/edit.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Create a new position
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('positions.update', $position) }}" method="post" >
+                        @method('PUT')
+                        @csrf
+                        <div class="mb-4">
+                            <label for="name" class="sr-only">Position Name</label>
+                            <input type="text" name="name" id="name" placeholder="Position Name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{ $position->name }}">
+
+                            @error('name')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div>
+                            <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
