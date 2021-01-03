755683d078ae20945b638131c267d582f68709e2
Lvzbel <rcoto@protonmail.com>
2021-01-03 01:40:40 -0800

    Feat: added links for Employees and positions

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('employees')" :active="request()->routeIs('employees*')">
+                        {{ __('Employees') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('positions')" :active="request()->routeIs('positions*')">
+                        {{ __('Positions') }}
+                    </x-nav-link>
                 </div>
             </div>
 
