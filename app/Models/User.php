e3aca2a2afbd1e96cf9cfc8e4b4f051c2eb33d4c
Lvzbel <rcoto@protonmail.com>
2021-01-02 22:30:05 -0800

    Fixed belongs to many relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,22 +34,13 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function schedule()
+    public function schedules()
     {
         return $this->hasMany(Schedule::class);
     }
 
-    public function position()
+    public function positions()
     {
-        $this->belongsToMany(Position::class);
+        return $this->belongsToMany(Position::class);
     }
 }
