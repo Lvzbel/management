1b8a1df08e0865a7a7536ec99c525ff787363179
Lvzbel <rcoto@protonmail.com>
2021-01-03 01:50:38 -0800

    Feat: added a role field to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('phone');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('employee');
             $table->rememberToken();
             $table->timestamps();
         });
