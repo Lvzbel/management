209f8dc8aeecdbae26363a1b85e25759d8956755
Lvzbel <rcoto@protonmail.com>
2021-01-03 20:49:50 -0800

    Feat: added admin account and availabilities to all user in seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(15)->create();
+        $admin = new User;
+        $admin->name = 'admin';
+        $admin->email = 'admin@test.com';
+        $admin->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+        $admin->role = 'admin';
+        $admin->save();
+
+        User::factory(15)->create();
+
+        // Add Availability Table to each user
+        User::all()->each(function ($user) {
+            $user->availability()->create();
+        });
     }
 }
